45039f15ca12c6cbf30514c09f29fcebafc2761a
guillermo webster <guillermo@cdbzb.com>
2018-10-30 23:32:43 -0700

    init

---
+++ index.js
@@ -0,0 +1,2 @@
+var randomColor = () => "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+// document.querySelectorAll('a').forEach(a => a.style.color = randomColor())
